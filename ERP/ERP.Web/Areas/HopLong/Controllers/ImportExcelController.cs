82f7da965865fdd83d42b163cb35e257b761bb4f
lamhien195 <lamhien2901@gmail.com>
2017-02-17 05:41:30 +0700

    lamhien-cm1-17022017

--- ERP/ERP.Web/Areas/HopLong/Controllers/ImportExcelController.cs
+++ ERP/ERP.Web/Areas/HopLong/Controllers/ImportExcelController.cs
@@ -1,4 +1,5 @@
 ﻿using ERP.Web.Models.Database;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -27,102 +28,61 @@ namespace ERP.Web.Areas.HopLong.Controllers
         {
             try
             {
-                DataSet ds = new DataSet();
-                if (Request.Files["file"].ContentLength > 0)
+                if (Request != null)
                 {
-                    string fileExtension = System.IO.Path.GetExtension(Request.Files["file"].FileName);
-
-                    if (fileExtension == ".xls" || fileExtension == ".xlsx")
+                    HttpPostedFileBase filetonkho = Request.Files["UploadedFile"];
+                    if ((filetonkho != null) && (filetonkho.ContentLength > 0) && !string.IsNullOrEmpty(filetonkho.FileName))
                     {
-                        string fileLocation = Server.MapPath("~/Content/") + Request.Files["file"].FileName;
-                        if (System.IO.File.Exists(fileLocation))
+                        string fileName = filetonkho.FileName;
+                        string fileContentType = filetonkho.ContentType;
+                        byte[] fileBytes = new byte[filetonkho.ContentLength];
+                        var data = filetonkho.InputStream.Read(fileBytes, 0, Convert.ToInt32(filetonkho.ContentLength));
+                        //var usersList = new List<Users>();
+                        using (var package = new ExcelPackage(filetonkho.InputStream))
                         {
+                            var currentSheet = package.Workbook.Worksheets;
+                            var workSheet = currentSheet.First();
+                            var noOfCol = workSheet.Dimension.End.Column;
+                            var noOfRow = workSheet.Dimension.End.Row;
+                            for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                            {
 
-                            //System.IO.File.Delete(fileLocation);
-                        }
-                        Request.Files["file"].SaveAs(fileLocation);
-                        string excelConnectionString = string.Empty;
-                        excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
-                        //connection String for xls file format.
-                        if (fileExtension == ".xls")
-                        {
-                            excelConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
-                        }
-                        //connection String for xlsx file format.
-                        else if (fileExtension == ".xlsx")
-                        {
+                                DM_HANG_HOA HH = new DM_HANG_HOA();
+                                HH.MA_HANG_HT = workSheet.Cells[rowIterator, 1].Value.ToString();
+                                HH.MA_HANG_NHAP = workSheet.Cells[rowIterator, 2].Value.ToString();
+                                if(workSheet.Cells[rowIterator, 3].Value != null)
+                                    HH.TEN_HANG = workSheet.Cells[rowIterator, 3].Value.ToString();
+                                HH.MA_NHOM_HANG = workSheet.Cells[rowIterator, 4].Value.ToString();
+                                if (workSheet.Cells[rowIterator, 5].Value != null)
+                                    HH.SERI = workSheet.Cells[rowIterator, 5].Value.ToString();
+                                if (workSheet.Cells[rowIterator, 6].Value != null)
+                                    HH.DON_VI_TINH = workSheet.Cells[rowIterator, 6].Value.ToString();
+                                if (workSheet.Cells[rowIterator, 7].Value != null)
+                                    HH.XUAT_XU = workSheet.Cells[rowIterator, 7].Value.ToString();
+                                if(workSheet.Cells[rowIterator, 8].Value != null)
+                                    HH.MODEL_DAC_BIET = Convert.ToBoolean(workSheet.Cells[rowIterator, 8].Value.ToString());
+                                if(workSheet.Cells[rowIterator, 9].Value != null)
+                                    HH.HINH_ANH = workSheet.Cells[rowIterator, 9].Value.ToString();
+                                if (workSheet.Cells[rowIterator, 10].Value != null)
+                                    HH.DAC_TINH = workSheet.Cells[rowIterator, 10].Value.ToString();
+                                if (workSheet.Cells[rowIterator, 11].Value != null)
+                                    HH.GHI_CHU = workSheet.Cells[rowIterator, 11].Value.ToString();
+                                if (workSheet.Cells[rowIterator, 12].Value != null)
+                                    HH.TK_HACH_TOAN_KHO = Convert.ToInt32(workSheet.Cells[rowIterator, 12].Value.ToString());
+                                if (workSheet.Cells[rowIterator, 13].Value != null)
+                                    HH.TK_DOANH_THU = Convert.ToInt32(workSheet.Cells[rowIterator, 13].Value.ToString());
+                                if (workSheet.Cells[rowIterator, 14].Value != null)
+                                    HH.TK_CHI_PHI = Convert.ToInt32(workSheet.Cells[rowIterator, 14].Value.ToString());
 
-                            excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
-                        }
-                        //Create Connection to Excel work book and add oledb namespace
-                        OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
-                        excelConnection.Open();
-                        DataTable dt = new DataTable();
+                                db.DM_HANG_HOA.Add(HH);
 
-                        dt = excelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                        if (dt == null)
-                        {
-                            return null;
-                        }
+                                db.SaveChanges();
+                                so_dong_thanh_cong++;
+                                dong = rowIterator - 1;
+                            }
 
-                        String[] excelSheets = new String[dt.Rows.Count];
-                        int t = 0;
-                        //excel data saves in temp file here.
-                        foreach (DataRow row in dt.Rows)
-                        {
-                            excelSheets[t] = row["TABLE_NAME"].ToString();
-                            t++;
-                        }
-                        OleDbConnection excelConnection1 = new OleDbConnection(excelConnectionString);
-
-
-                        string query = string.Format("Select * from [{0}]", excelSheets[0]);
-                        using (OleDbDataAdapter dataAdapter = new OleDbDataAdapter(query, excelConnection1))
-                        {
-                            dataAdapter.Fill(ds);
                         }
                     }
-                    if (fileExtension.ToString().ToLower().Equals(".xml"))
-                    {
-                        string fileLocation = Server.MapPath("~/Content/") + Request.Files["FileUpload"].FileName;
-                        if (System.IO.File.Exists(fileLocation))
-                        {
-                            System.IO.File.Delete(fileLocation);
-                        }
-
-                        Request.Files["FileUpload"].SaveAs(fileLocation);
-                        XmlTextReader xmlreader = new XmlTextReader(fileLocation);
-                        // DataSet ds = new DataSet();
-                        ds.ReadXml(xmlreader);
-                        xmlreader.Close();
-                    }
-                    so_dong_thanh_cong = 0;
-                    dong = 0;
-                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                    {
-                        DM_HANG_HOA HH = new DM_HANG_HOA();
-                        HH.MA_HANG_HT = ds.Tables[0].Rows[i][0].ToString();
-                        HH.MA_HANG_NHAP = ds.Tables[0].Rows[i][1].ToString();
-                        HH.TEN_HANG = ds.Tables[0].Rows[i][2].ToString();
-                        HH.MA_NHOM_HANG = ds.Tables[0].Rows[i][3].ToString();
-                        HH.SERI = ds.Tables[0].Rows[i][4].ToString();
-                        HH.DON_VI_TINH = ds.Tables[0].Rows[i][5].ToString();
-                        HH.XUAT_XU = ds.Tables[0].Rows[i][6].ToString();
-                        HH.MODEL_DAC_BIET= Convert.ToBoolean(ds.Tables[0].Rows[i][7]);
-                        HH.HINH_ANH = ds.Tables[0].Rows[i][8].ToString();
-                        HH.DAC_TINH = ds.Tables[0].Rows[i][9].ToString();
-                        HH.GHI_CHU = ds.Tables[0].Rows[i][10].ToString();
-                        HH.TK_HACH_TOAN_KHO = Convert.ToInt32(ds.Tables[0].Rows[i][11]);
-                        HH.TK_DOANH_THU = Convert.ToInt32(ds.Tables[0].Rows[i][12]);
-                        HH.TK_CHI_PHI = Convert.ToInt32(ds.Tables[0].Rows[i][13]);
-
-                        db.DM_HANG_HOA.Add(HH);
-
-                        db.SaveChanges();
-                        so_dong_thanh_cong++;
-                        dong = i;
-                    }
-
                 }
             }
             catch (Exception Ex)
@@ -151,91 +111,38 @@ namespace ERP.Web.Areas.HopLong.Controllers
         {
             try
             {
-                DataSet ds = new DataSet();
-                if (Request.Files["file"].ContentLength > 0)
+                if (Request != null)
                 {
-                    string fileExtension = System.IO.Path.GetExtension(Request.Files["file"].FileName);
-
-                    if (fileExtension == ".xls" || fileExtension == ".xlsx")
+                    HttpPostedFileBase filetonkho = Request.Files["UploadedFile"];
+                    if ((filetonkho != null) && (filetonkho.ContentLength > 0) && !string.IsNullOrEmpty(filetonkho.FileName))
                     {
-                        string fileLocation = Server.MapPath("~/Content/") + Request.Files["file"].FileName;
-                        if (System.IO.File.Exists(fileLocation))
+                        string fileName = filetonkho.FileName;
+                        string fileContentType = filetonkho.ContentType;
+                        byte[] fileBytes = new byte[filetonkho.ContentLength];
+                        var data = filetonkho.InputStream.Read(fileBytes, 0, Convert.ToInt32(filetonkho.ContentLength));
+                        //var usersList = new List<Users>();
+                        using (var package = new ExcelPackage(filetonkho.InputStream))
                         {
+                            var currentSheet = package.Workbook.Worksheets;
+                            var workSheet = currentSheet.First();
+                            var noOfCol = workSheet.Dimension.End.Column;
+                            var noOfRow = workSheet.Dimension.End.Row;
+                            for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                            {
+                                DM_TONKHO_HANG HH = new DM_TONKHO_HANG();
+                                HH.MA_HANG_HT = workSheet.Cells[rowIterator, 1].Value.ToString();
+                                HH.MA_NHOM_HANG = workSheet.Cells[rowIterator, 2].Value.ToString();
+                                HH.SL_TON = Convert.ToInt32(workSheet.Cells[rowIterator, 3].Value.ToString());
 
-                            //System.IO.File.Delete(fileLocation);
-                        }
-                        Request.Files["file"].SaveAs(fileLocation);
-                        string excelConnectionString = string.Empty;
-                        excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
-                        //connection String for xls file format.
-                        if (fileExtension == ".xls")
-                        {
-                            excelConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
-                        }
-                        //connection String for xlsx file format.
-                        else if (fileExtension == ".xlsx")
-                        {
+                                db.DM_TONKHO_HANG.Add(HH);
 
-                            excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
-                        }
-                        //Create Connection to Excel work book and add oledb namespace
-                        OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
-                        excelConnection.Open();
-                        DataTable dt = new DataTable();
+                                db.SaveChanges();
+                                so_dong_thanh_cong++;
+                                dong = rowIterator - 1;
+                            }
 
-                        dt = excelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                        if (dt == null)
-                        {
-                            return null;
-                        }
-
-                        String[] excelSheets = new String[dt.Rows.Count];
-                        int t = 0;
-                        //excel data saves in temp file here.
-                        foreach (DataRow row in dt.Rows)
-                        {
-                            excelSheets[t] = row["TABLE_NAME"].ToString();
-                            t++;
-                        }
-                        OleDbConnection excelConnection1 = new OleDbConnection(excelConnectionString);
-
-
-                        string query = string.Format("Select * from [{0}]", excelSheets[0]);
-                        using (OleDbDataAdapter dataAdapter = new OleDbDataAdapter(query, excelConnection1))
-                        {
-                            dataAdapter.Fill(ds);
                         }
                     }
-                    if (fileExtension.ToString().ToLower().Equals(".xml"))
-                    {
-                        string fileLocation = Server.MapPath("~/Content/") + Request.Files["FileUpload"].FileName;
-                        if (System.IO.File.Exists(fileLocation))
-                        {
-                            System.IO.File.Delete(fileLocation);
-                        }
-
-                        Request.Files["FileUpload"].SaveAs(fileLocation);
-                        XmlTextReader xmlreader = new XmlTextReader(fileLocation);
-                        // DataSet ds = new DataSet();
-                        ds.ReadXml(xmlreader);
-                        xmlreader.Close();
-                    }
-                    so_dong_thanh_cong = 0;
-                    dong = 0;
-                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                    {
-                        DM_TONKHO_HANG HH = new DM_TONKHO_HANG();
-                        HH.MA_HANG_HT = ds.Tables[0].Rows[i][0].ToString();
-                        HH.MA_NHOM_HANG = ds.Tables[0].Rows[i][1].ToString();
-                        HH.SL_TON = Convert.ToInt32(ds.Tables[0].Rows[i][2]);
-
-                        db.DM_TONKHO_HANG.Add(HH);
-
-                        db.SaveChanges();
-                        so_dong_thanh_cong++;
-                        dong = i;
-                    }
-
                 }
             }
             catch (Exception Ex)
@@ -252,6 +159,66 @@ namespace ERP.Web.Areas.HopLong.Controllers
             return View();
         }
 
+        #endregion
+
+        #region "Import Phòng Ban"
+        public ActionResult Import_PhongBan()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Import_PhongBan(FormCollection formCollection)
+        {
+            try
+            {
+                if (Request != null)
+                {
+                    HttpPostedFileBase file = Request.Files["UploadedFile"];
+                    if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))   
+                     {
+                        string fileName = file.FileName;
+                        string fileContentType = file.ContentType;
+                        byte[] fileBytes = new byte[file.ContentLength];
+                        var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                        //var usersList = new List<Users>();
+                        using (var package = new ExcelPackage(file.InputStream))
+                        {
+                            var currentSheet = package.Workbook.Worksheets;
+                            var workSheet = currentSheet.First();
+                            var noOfCol = workSheet.Dimension.End.Column;
+                            var noOfRow = workSheet.Dimension.End.Row;
+                            for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)   
+                                {
+                                    CCTC_PHONG_BAN phongban = new CCTC_PHONG_BAN();
+                                    phongban.MA_PHONG_BAN = workSheet.Cells[rowIterator, 1].Value.ToString();
+                                    phongban.TEN_PHONG_BAN = workSheet.Cells[rowIterator, 2].Value.ToString();
+                                    if(workSheet.Cells[rowIterator, 3].Value != null)
+                                        phongban.SDT = workSheet.Cells[rowIterator, 3].Value.ToString();
+                                    phongban.MA_CONG_TY = workSheet.Cells[rowIterator, 4].Value.ToString();
+                                    if (workSheet.Cells[rowIterator, 5].Value != null)
+                                        phongban.GHI_CHU = workSheet.Cells[rowIterator, 5].Value.ToString();
+                                    db.CCTC_PHONG_BAN.Add(phongban);
+
+                                db.SaveChanges();
+                                so_dong_thanh_cong++;
+                                dong = rowIterator-1;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                ViewBag.Error = " Đã xảy ra lỗi, Liên hệ ngay với admin. " + Environment.NewLine + " Thông tin chi tiết về lỗi:" + Environment.NewLine + Ex;
+                ViewBag.Information = "Lỗi tại dòng thứ: " + dong;
+
+            }
+            finally
+            {
+                ViewBag.Message = "Đã import thành công " + so_dong_thanh_cong + " dòng";
+            }
+            return View("Import_Hanghoa");
+        }
         #endregion
     }
 }
